6118cee9e958118f24a35a532a04f3819e5a3ac3
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-10-22 16:27:33 +0900

    コメントアウト消去

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,19 +1,15 @@
 class CalendarsController < ApplicationController
-
-  # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
     @plan = Plan.new
   end
 
-  # 予定の保存
   def create
     Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
-
   def plan_params
     params.require(:plan).permit(:date, :plan)
   end
@@ -21,9 +17,7 @@ class CalendarsController < ApplicationController
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
